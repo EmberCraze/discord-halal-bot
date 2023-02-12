b52b5762eb0c1bec4a74fd97e40c524b0a696f0f
EmberCraze <maher.shaker@live.se>
2023-02-12 01:04:40 +0100

    add an action that gives a random name of allah and its description

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
 import os
+import requests
+import random
 from typing import Literal, Union, NamedTuple
 from enum import Enum
 
@@ -58,8 +60,8 @@ async def quran_leaderboard(interaction: discord.Interaction):
     await interaction.response.send_message(embed=embed)
 
 
-@client.tree.command()
-@app_commands.describe(page="The page were you are it in your reading")
+@client.tree.command(name="update-reading-progress")
+@app_commands.describe(page="The lastest page you have completed reading")
 async def update_reading_progress(
     interaction: discord.Interaction,
     # This makes it so the page parameter can only be between 0 to 604.
@@ -77,6 +79,44 @@ async def update_reading_progress(
     db.close()
 
     await interaction.response.send_message(f"Your page progress is now set to {page}")
+
+
+@client.tree.command(name="a-random-name-of-allah")
+async def random_name_of_allah(interaction: discord.Interaction):
+    """Get a random name of Allah (SWT)"""
+
+    random_number = random.randint(1, 99)
+
+    # Make a GET request to the API endpoint
+    response = requests.get(f"https://api.aladhan.com/v1/asmaAlHusna/{random_number}")
+
+    name = ""
+    transliteration = ""
+    meaning = ""
+
+    # Check the status code of the response to make sure the request was successful
+    if response.status_code == 200:
+        # Get the JSON data from the response
+        response = response.json()
+        data = response.get("data")[0]
+
+        name = data.get("name")
+        transliteration = data.get("transliteration")
+        meaning = data.get("en").get("meaning")
+    else:
+        await interaction.response.send_message("Something went wront!")
+        return
+
+    embed = discord.Embed()
+
+    embed.add_field(name="Name", value=name, inline=True)
+    embed.add_field(name="Transliteration", value=transliteration, inline=True)
+    embed.add_field(name="Meaning", value=meaning, inline=True)
+
+    # Add spacer
+    embed.add_field(name="", value="", inline=False)
+
+    await interaction.response.send_message(embed=embed)
 
 
 client.run(TOKEN)
